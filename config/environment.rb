96e78bb72eaa7c8a971791bf54ad313c81c0378b
Tony Blum <tonyblum@me.com>
2020-12-01 07:32:04 -0800

    pg config

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,19 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# configure :development do
+#   set :database, "sqlite3:///database.db"
+# end
+#
+# configure :production do
+#   db = URI.parse(ENV['HEROKU_POSTGRESQL_JADE_URL'] || 'postgres://localhost/hobby-dev')
+#
+#   ActiveRecord::Base.establish_connection(
+#     :adapter    => db.sheme == 'postgres' ? 'postgresql' : db.scheme,
+#     :host       => db.host,
+#     :username   => db.user,
+#     :password   => db.password[1..-1],
+#     :encoding   => 'utf8'
+#   )
+# end
