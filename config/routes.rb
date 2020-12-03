98fbcf262bcc00d03608ff17a7d247bfeb80e0c2
Tony Blum <tonyblum@me.com>
2020-12-03 02:14:32 -0800

    Authors/Posts added, bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
 
   devise_for :authors
+
   get 'home/index'
-   root 'home#index'
+
+  root 'home#index'
+
+  scope module: 'authors' do
+  resources :posts
+  end
 
 end
